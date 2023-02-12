9f44aa4303d6cad241e62f201f4609ee54e51c33
yberstro <alfianzikri140@gmail.com>
2023-02-12 21:15:54 +0700

    Memnbuat view index angkatan dan jurusan

--- app/Http/Controllers/AngkatanController.php
+++ app/Http/Controllers/AngkatanController.php
@@ -13,7 +13,7 @@ class AngkatanController extends Controller
      */
     public function index()
     {
-        //
+        return view('angkatan.index');
     }
 
     /**
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -276,7 +276,7 @@
         </div>
       </div><!-- /.container-fluid -->
     </section> --}}
-    @yield('conten-header') 
+    @yield('content-header') 
     <!-- Main content -->
     {{-- <section class="content">
 
